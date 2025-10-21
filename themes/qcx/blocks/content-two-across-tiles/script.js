5a5b0a3a9fec3878e3af4a0cdb88fb57b0bde114
SteveBaunfire <sfelizardo@baunfire.com>
2025-10-22 05:21:29 +0800

    update block layout

--- themes/qcx/blocks/content-two-across-tiles/script.js
+++ themes/qcx/blocks/content-two-across-tiles/script.js
@@ -8,7 +8,44 @@ baunfire.addModule({
 
             els.each(function () {
                 const self = $(this);
-                /* Add your logic here */
+                const leftColumn = self.find('.left-card');
+                const rightColumn = self.find('.rigth-card');
+                
+                const mm = gsap.matchMedia();
+                
+                mm.add("(min-width: 768px)", () => {
+                    gsap.fromTo(leftColumn, 
+                        {
+                            yPercent: 15
+                        },
+                        {
+                            yPercent: -15,
+                            ease: "none",
+                            scrollTrigger: {
+                                trigger: self,
+                                start: "top bottom",
+                                end: "bottom top",
+                                scrub: 1.5
+                            }
+                        }
+                    );
+                    
+                    gsap.fromTo(rightColumn,
+                        {
+                            yPercent: 15
+                        },
+                        {
+                            yPercent: -20,
+                            ease: "none",
+                            scrollTrigger: {
+                                trigger: self,
+                                start: "top bottom",
+                                end: "bottom top",
+                                scrub: 1
+                            }
+                        }
+                    );
+                });
             });
         }
 
