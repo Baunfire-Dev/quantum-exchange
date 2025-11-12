76c7f18b0f48b4804aa5305796347ba50c6d4ea2
Rich <241861127+rcuer-pandoblox@users.noreply.github.com>
2025-11-12 23:42:36 +0800

    updates to build

---
+++ themes/qcx/blocks/content-large-diagram-graphic/script.js
@@ -0,0 +1,71 @@
+baunfire.addModule({
+    init(baunfire) {
+        const $ = baunfire.$;
+
+        const script = () => {
+            const sections = $("section.content-large-diagram-graphic");
+            if (!sections.length) return;
+
+            sections.each(function () {
+                const self = $(this);
+                handleVideos(self);
+                handleEntranceAnim(self);
+            });
+        };
+
+        const handleEntranceAnim = (self) => {
+            const title = self.find(".block-title");
+            const para = self.find(".block-para");
+            const media = self.find(".media-container-outer");
+
+            const entranceAnim = gsap.timeline({
+                scrollTrigger: {
+                    trigger: self,
+                    start: baunfire.anim.start
+                }
+            })
+                .fromTo([title, para, media],
+                    {
+                        y: 40,
+                        autoAlpha: 0
+                    },
+                    {
+                        y: 0,
+                        autoAlpha: 1,
+                        duration: 0.8,
+                        stagger: { each: 0.2 },
+                        ease: Power2.easeOut
+                    }
+                );
+        };
+
+        const handleVideos = (self) => {
+            const videoContainers = self.find(".media-container.video");
+            if (!videoContainers.length) return;
+
+            videoContainers.each(function (index) {
+                const subSelf = $(this);
+                setupVideo(self, subSelf);
+            });
+        };
+
+        const setupVideo = (self, videoContainer) => {
+            const videoRaw = videoContainer.find("video");
+            if (!videoRaw.length) return;
+
+            const video = videoRaw.get(0);
+
+            ScrollTrigger.create({
+                trigger: videoContainer,
+                start: "top center",
+                end: "bottom 30%",
+                onEnter: () => video.play(),
+                onEnterBack: () => video.play(),
+                onLeave: () => video.pause(),
+                onLeaveBack: () => video.pause()
+            });
+        };
+
+        script();
+    },
+});
