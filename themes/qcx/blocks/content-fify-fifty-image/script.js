2c42135768e6fd7797670ee0a5c59b929deab9fa
Rich <rcuer@baunfire.com>
2025-11-18 06:50:01 +0800

    build updates

--- themes/qcx/blocks/content-fify-fifty-image/script.js
+++ themes/qcx/blocks/content-fify-fifty-image/script.js
@@ -8,9 +8,37 @@ baunfire.addModule({
 
             els.each(function () {
                 const self = $(this);
-                /* Add your logic here */
+                handleEntranceAnim(self);
             });
         }
+
+        const handleEntranceAnim = (self) => {
+            const title = self.find(".block-title");
+            const content = self.find(".block-content");
+            const image = self.find(".block-image");
+
+            const elAnims = [image, title, content].filter(el => el.length > 0);
+
+            const entranceAnim = gsap.timeline({
+                scrollTrigger: {
+                    trigger: self,
+                    start: baunfire.anim.start
+                }
+            })
+                .fromTo(elAnims,
+                    {
+                        y: 40,
+                        autoAlpha: 0
+                    },
+                    {
+                        y: 0,
+                        autoAlpha: 1,
+                        duration: 0.8,
+                        stagger: { each: 0.2 },
+                        ease: "power2.out"
+                    }
+                );
+        };
 
         script();
     }
