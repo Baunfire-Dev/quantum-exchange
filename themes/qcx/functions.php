757b38418870209ca5cf2efdf5d0f3684d61efe3
rcuer <rcuer@baunfire.com>
2026-02-12 04:45:04 +0800

    build updates

--- themes/qcx/functions.php
+++ themes/qcx/functions.php
@@ -27,7 +27,7 @@ Timber\Timber::init();
 //         define('_S_VERSION', uniqid());
 // }
 
-define('_S_VERSION', '1.0.6');
+define('_S_VERSION', '1.0.7');
 
 if (!function_exists('bf_setup')):
     function bf_setup()
