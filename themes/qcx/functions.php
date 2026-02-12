ce1279ad5cf944535e1ad3640fcfa30062c4c2fd
rcuer <rcuer@baunfire.com>
2026-02-12 08:48:00 +0800

    build updates

--- themes/qcx/functions.php
+++ themes/qcx/functions.php
@@ -145,9 +145,17 @@ function front_deferred_styles($html, $handle, $href, $media) {
     return $html;
 }
 
+function remove_jquery_migrate($scripts) {
+    if (!is_admin() && isset($scripts->registered['jquery'])) {
+        $script = $scripts->registered['jquery'];
 
+        if ($script->deps) {
+            $script->deps = array_diff($script->deps, array('jquery-migrate'));
+        }
+    }
+}
 
-
+add_action('wp_default_scripts', 'remove_jquery_migrate');
 
 
 
