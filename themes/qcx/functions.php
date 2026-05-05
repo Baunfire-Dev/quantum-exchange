fc5e242b22cfbccfa339b91f1c70b0d62735fa79
rcuer <rcuer@baunfire.com>
2026-05-06 02:03:48 +0800

    updates to build

--- themes/qcx/functions.php
+++ themes/qcx/functions.php
@@ -16,7 +16,7 @@ if (!defined('ABSPATH')) {
 require_once 'vendor/autoload.php';
 Timber\Timber::init();
 
-define('_S_VERSION', '1.2.0');
+define('_S_VERSION', '1.2.1');
 
 if (!function_exists('bf_stup')):
     function bf_setup()
